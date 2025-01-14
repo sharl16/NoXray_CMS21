e3e26333fe8d204cf24fe5a640b19cd5c4cda28a
sharl16 <nickonlyforaccs@gmail.com>
2025-01-15 00:10:38 +0200

    Initial Commit

---
+++ CMS21_Mod/NoXray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+
+namespace CMS21_Mod
+{
+    public class NoXray : MelonMod
+    {
+        private static bool state = true;
+
+        [HarmonyPatch(typeof(CarLoader))]
+        [HarmonyPatch("ShowHideBody", new Type[] { typeof(bool), typeof(float)})]
+        public class Patch
+        {
+            public static bool Prefix(ref bool show, ref float XRayAlpha)
+            {
+                show = state; //Hide XRay?
+                MelonLogger.Msg("Hide Xray? " + show);
+                return true;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                state = !state;
+                MelonLogger.Msg("Changed XRay state: " + state);
+            }
+        }
+    }
+}
